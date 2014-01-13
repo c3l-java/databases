79c738603a0cc5fcbcbd2ca1c3b09c88688ed0f0
Tom Blackmore <tablackmore@gmail.com>
2014-01-13 12:11:46 +0100

    added completed exercise

---
+++ resources/TestConnection.java
@@ -0,0 +1,74 @@
+package floggit;
+
+import java.sql.*;
+
+public class TestConnection {
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/floggit";
+
+	// Database credentials
+	static final String USER = "root";
+	static final String PASS = "";
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT id, firstname, surname FROM staff";
+			rs = stmt.executeQuery(sql);
+
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int id = rs.getInt("id");
+				String first = rs.getString("firstname");
+				String last = rs.getString("surname");
+
+				// Display values
+				System.out.print("ID: " + id);
+				System.out.print(", First: " + first);
+				System.out.println(", Last: " + last);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}// end try
+		System.out.println("Goodbye!");
+	}// end main
+}// end FirstExample
